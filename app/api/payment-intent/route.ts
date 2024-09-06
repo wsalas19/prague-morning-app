bd264563048302ff920f14cf2be52e70cdfb4db2
wsalas19 <willbullet52@gmail.com>
2024-09-06 12:35:37 -0500

    hotfix: route definition fix

--- app/api/payment-intent/route.ts
+++ app/api/payment-intent/route.ts
@@ -1,5 +1,7 @@
-// This is your test secret API key.
-const stripe = require("stripe")(process.env.STRIPE_API_KEY);
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_API_KEY!);
 
 const calculateOrderAmount = () => {
 	// Replace this constant with a calculation of the order's amount
@@ -8,20 +10,24 @@ const calculateOrderAmount = () => {
 	return 1400;
 };
 
-export default async function POST(req: Request, res: Response) {
-	// Create a PaymentIntent with the order amount and currency
-	const paymentIntent = await stripe.paymentIntents.create({
-		amount: calculateOrderAmount(),
-		currency: "usd",
-		// In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-		automatic_payment_methods: {
-			enabled: true,
-		},
-	});
+export async function POST(req: Request) {
+	try {
+		// Create a PaymentIntent with the order amount and currency
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount: calculateOrderAmount(),
+			currency: "usd",
+			// In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+			automatic_payment_methods: {
+				enabled: true,
+			},
+		});
 
-	return Response.json({
-		clientSecret: paymentIntent.client_secret,
-		// [DEV]: For demo purposes only, you should avoid exposing the PaymentIntent ID in the client-side code.
-		dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
-	});
+		return Response.json({
+			clientSecret: paymentIntent.client_secret,
+			// [DEV]: For demo purposes only, you should avoid exposing the PaymentIntent ID in the client-side code.
+			dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
+		});
+	} catch (error: unknown) {
+		return NextResponse.json({ error: error, message: "Internal server error" }, { status: 500 });
+	}
 }
