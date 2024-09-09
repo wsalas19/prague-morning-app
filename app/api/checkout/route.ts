888dc5b841e9fed90ab71b70a6d8f4f5e66ab015
wsalas19 <willbullet52@gmail.com>
2024-09-09 16:53:24 -0500

    feature: new payment logic

---
+++ app/api/checkout/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_API_KEY!);
+
+export async function POST(req: Request) {
+	const body = await req.json();
+
+	try {
+		const session = await stripe.checkout.sessions.create({
+			mode: "payment",
+			line_items: [
+				{
+					price_data: {
+						currency: "usd",
+						product_data: {
+							name: body.title,
+						},
+						unit_amount: body.price * 100,
+					},
+					quantity: 1,
+				},
+			],
+			success_url: `${process.env.NEXT_PUBLIC_APP_URL!}/success`,
+			cancel_url: `${process.env.NEXT_PUBLIC_APP_URL!}/cancel`,
+		});
+		return NextResponse.json({ session });
+	} catch (error: unknown) {
+		return NextResponse.json(
+			{ error: error, message: "Could not create checkout session" },
+			{ status: 500 },
+		);
+	}
+}
--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,24 +1,33 @@
-'use client'
-import React, {useState} from 'react';
-import styles from "./paymenPage.module.scss"
+"use client";
+import React, { useState } from "react";
+import styles from "./paymenPage.module.scss";
 import PaymentPlanCheckbox from "@/lib/components/payment/paymentPlanCheckbox/paymentPlanCheckbox";
 import ServicePlans from "@/lib/components/servicePlans/servicePlans";
 import PaymentContainer from "@/lib/components/payment/paymentContainer/paymentContainer";
+import { ServicePlanType } from "@/lib/types/componentTypes";
 
 const Payment = () => {
-  const [monthly, setMonthly] = useState<boolean>(true)
-  const [yearly, setYearly] = useState<boolean>(false)
-  const [servicePlan,setServicePlan]=useState<number | undefined>(150)
+	const [monthly, setMonthly] = useState<boolean>(true);
+	const [yearly, setYearly] = useState<boolean>(false);
+	const [servicePlan, setServicePlan] = useState<ServicePlanType>({
+		title: "Bronze",
+		price: 0,
+	});
 
-  return (
-    <section className={styles["payment-page"]}>
-      <div className={styles["payment-page__wrapper"]}>
-        <PaymentPlanCheckbox monthly={monthly} yearly={yearly} setMonthly={setMonthly} setYearly={setYearly}/>
-        <ServicePlans servicePlan={servicePlan} setServicePlan={setServicePlan}/>
-        {!!servicePlan && <PaymentContainer servicePlan={servicePlan}/>}
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles["payment-page"]}>
+			<div className={styles["payment-page__wrapper"]}>
+				<PaymentPlanCheckbox
+					monthly={monthly}
+					yearly={yearly}
+					setMonthly={setMonthly}
+					setYearly={setYearly}
+				/>
+				<ServicePlans servicePlan={servicePlan} setServicePlan={setServicePlan} />
+				{!!servicePlan && <PaymentContainer {...servicePlan} />}
+			</div>
+		</section>
+	);
 };
 
 export default Payment;
